e327e92740137d7c6e353461a76d14ea862556fa
Caroline Wangari <karehwachira@gmail.com>
2024-04-18 02:41:34 +0300

    Cards

--- paw-prints/src/components/PetCard.js
+++ paw-prints/src/components/PetCard.js
@@ -6,13 +6,15 @@ function PetCard({ pet }) {
   return (
     <div className="pet-card">
       <img src={pet.image_url} alt={pet.name} className="pet-image" />
-      <div className="pet-name">{pet.name}</div>
-      <div className="pet-info">
-        <div>Species: {pet.species}</div>
-        <div>Breed: {pet.breed}</div>
-        <div>Age: {pet.age}</div>
-        <div>Size: {pet.size}</div>
-        <div>Gender: {pet.gender}</div>
+      <div className="pet-details">
+        <div className="pet-name">{pet.name}</div>
+        <div className="pet-info">
+          <div>Species: {pet.species}</div>
+          <div>Breed: {pet.breed}</div>
+          <div>Age: {pet.age}</div>
+          <div>Size: {pet.size}</div>
+          <div>Gender: {pet.gender}</div>
+        </div>
       </div>
     </div>
   );
